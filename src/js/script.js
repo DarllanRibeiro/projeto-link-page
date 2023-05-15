a57c124b5343cde952c9819984d49c74b9ac4cf7
Darllan Ribeiro <104278746+DarllanRibeiro@users.noreply.github.com>
2023-05-15 03:34:22 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+let interruptor= document.getElementById('interruptor')
+let root= document.documentElement
+interruptor.addEventListener('click', () =>{     
+
+root.classList.toggle('tema-claro')
+
+
+
+  })
